8281d9e6b37e2050ca5d163cddc573ce84bd0b62
luizfk <luiz.kessler7@gmail.com>
2022-09-15 15:41:05 -0300

    funcao2

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -4,3 +4,44 @@ const objeto = {
     username: "astrodev_labenu",
     senha: "melhorDeTodos"
 }
+// Crie duas funcões que recebem como parâmetro um objeto.
+// 1. A primeira deve retornar o objeto, alterando as propriedades dele para que fiquem em CAIXA ALTA.
+// 2. A segunda deve retornar os valores do objeto como texto corrido.
+
+// 3. Em seguida, crie uma funcão que recebe um objeto e um callback como parâmetros. A funcão deve passar o objeto como argumento da funcão de callback, e em seguida imprimir o valor retornado.
+
+// Chame a funcão 3, passando como argumentos o objeto passado no arquivo `script.js`, e a funcão 1.
+// Repita o processo para a funcão 2
+
+const maiusc = (objeto)=>{
+    return {
+        nome: objeto.nome.toUpperCase(),
+        profissao: objeto.profissao.toUpperCase(),
+        username: objeto.username.toUpperCase(),
+        senha: objeto.senha.toUpperCase()
+    }
+}
+console.log(maiusc(objeto))
+
+const junto= (objeto)=>{
+    return tudo =objeto.nome+objeto.profissao+objeto.username+objeto.senha
+}
+
+console.log(junto(objeto))
+
+const all =(objeto,callback)=>{
+    console.log(objeto)
+    console.log(callback)
+}
+all(objeto,maiusc(objeto))
+
+
+
+
+
+
+
+
+
+
+
